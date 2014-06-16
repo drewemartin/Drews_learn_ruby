054d94f2758c851dba069fe38a2bdd2b0493e959
Drew Martin <drew.e.martin@gmail.com>
2014-06-16 14:14:28 -0400

    added test example for self  reminder

---
+++ test_example.rb
@@ -0,0 +1,21 @@
+
+describe "the greet function" do #"greet is the name of the method"
+  it "says hello to someone" do #this describes what the "greet method should do"
+    greet("Alice").should == "Hello, Alice!"
+  end
+
+  it "says hello to someone else" do
+    greet("Bob").should == "Hello, Bob!"
+  end
+
+##########################################
+
+require 'file_that_contains_tested_method.rb'
+		
+			#EXAMPLE#
+
+  describe "name of method" do
+  	it "action that it does" do
+  		method.should == "what it should do"
+  	end
+  end
